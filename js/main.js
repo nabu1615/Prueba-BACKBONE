7c8c2dc753c166454095ec010dd8f577cc6b140d
Javier Medina <javiermlovo@hotmail.com>
2020-07-29 07:29:14 -0500

    Prueba Backbone, pagina home Paraiso Centr

---
+++ js/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+    $('#slideshow .slick').slick({
+        autoplay: true,
+        arrows: false,
+        dots: true,
+        speed: 2000,
+        autoplaySpeed: 4000,
+
+    });
+
+    $('#slideshow .slick-2').slick({
+        autoplay: true,
+        arrows: false,
+        dots: true,
+        autoplaySpeed: 4000,
+        speed: 2000
+    });
+
+
+    $('#slideshow .slick-3').slick({
+        autoplay: true,
+        arrows: false,
+        dots: true,
+        autoplaySpeed: 4000,
+        speed: 2000
+    });
+
+    $('#slideshow .slick-4').slick({
+        autoplay: true,
+        arrows: true,
+        dots: true,
+        autoplaySpeed: 4000,
+        speed: 2000
+    });
+
+
+
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: true,
+        dots: false,
+        autoplaySpeed: 2000,
+    });
+
+    $('.multiple-items-2').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: true,
+        dots: false,
+        autoplaySpeed: 2000,
+    });
+    //hiden menu mobile
+
+    $('.menu').on('click', function() {
+        $(this).hide();
+        $('.navigation').show();
+    });
+
+});
---
+++ js/map.js
@@ -0,0 +1,18 @@
+(function() {
+    'use strict';
+
+    document.addEventListener('DOMContentLoaded', function() {
+        console.log('ready');
+
+        var mymap = L.map('mapa').setView([3.459164, -76.519432], 16);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(mymap);
+
+        L.marker([3.459164, -76.519432]).addTo(mymap)
+            .bindPopup('Paraiso Central')
+            .openPopup('Paraiso Central');
+
+    }); //DOM CONTENT LOADED
+})();
